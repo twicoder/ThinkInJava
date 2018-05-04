f46b4244d39e3bfeeb3e9b0b61bbed3f30d70bb3
twicoder <renqingwei123@163.com>
2018-05-04 21:51:13 +0800

    demo of null object

---
+++ src/chinwe/ch14/typeinfo/Operation.java
@@ -0,0 +1,6 @@
+package chinwe.ch14.typeinfo;
+
+public interface Operation {
+    String description();
+    void command();
+}
---
+++ src/chinwe/ch14/typeinfo/Position.java
@@ -0,0 +1,43 @@
+package chinwe.ch14.typeinfo;
+
+public class Position {
+    private String title;
+    private Person person;
+
+    public Position(String title, Person person) {
+        this.title = title;
+        this.person = person;
+        if(person == null) {
+            this.person = Person.NULL;
+        }
+    }
+
+    public Position(String jobTitle){
+        title = jobTitle;
+        person = Person.NULL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+        if(person == null){
+            this.person = Person.NULL;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Position:" + title + " " + person;
+    }
+}
