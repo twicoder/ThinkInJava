8c266e102ee9f494ef9e1cbffe1b5fb24483db9e
twicoder <renqingwei123@163.com>
2018-05-05 13:01:40 +0800

    use generator to generate object which has private constructor

---
+++ src/com/chinwe/ch15/generics/BankTeller.java
@@ -0,0 +1,56 @@
+package com.chinwe.ch15.generics;
+
+import net.mindview.util.Generator;
+
+import java.util.*;
+
+class Customer {
+    private static long counter = 1;
+    private final long id = counter++;
+    private Customer() {}
+    public String toString() {
+        return "Customer " + id;
+    }
+    // A method to produce Generator objects:
+    public static Generator<Customer> generator() {
+        return new Generator<Customer>() {
+            public Customer next() {
+                return new Customer();
+            }
+        };
+    }
+}
+
+class Teller {
+    private static long counter = 1;
+    private final long id = counter++;
+    private Teller() {}
+    public String toString() {
+        return "Teller " + id;
+    }
+    // A single Generator object:
+    public static Generator<Teller> generator = new Generator<Teller>(){
+        @Override
+        public Teller next() {
+            return new Teller();
+        }
+    };
+}
+
+
+public class BankTeller {
+    public static void serve(Teller t, Customer c) {
+        System.out.println(t + " serves " + c);
+    }
+
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        Queue<Customer> line = new LinkedList<Customer>();
+        Generators.fill(line, Customer.generator(),15);
+        List<Teller> tellers = new ArrayList<>();
+        Generators.fill(tellers, Teller.generator, 4);
+        for(Customer c : line) {
+            serve(tellers.get(rand.nextInt(tellers.size())),c);
+        }
+    }
+}
