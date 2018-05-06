a90f4db2a9fb23d0dcaf9f2e2e79c8522605bb84
twicoder <renqingwei123@163.com>
2018-05-06 21:10:34 +0800

    add EnumSet and EnumMap

---
+++ src/com/chinwe/ch19/enumerated/AlarmPoints.java
@@ -0,0 +1,6 @@
+package com.chinwe.ch19.enumerated;
+
+public enum AlarmPoints {
+    STAIR1,STAIR2,LOBBY,OFFICE1,OFFICE2,OFFICE3,
+    OFFICE4,BATHROOM,UTILITY,KITCHEN
+}
---
+++ src/com/chinwe/ch19/enumerated/EnumSets.java
@@ -0,0 +1,22 @@
+package com.chinwe.ch19.enumerated;
+
+import java.util.EnumSet;
+
+import static com.chinwe.ch19.enumerated.AlarmPoints.*;
+
+public class EnumSets {
+    public static void main(String[] args) {
+        EnumSet<AlarmPoints> points = EnumSet.noneOf(AlarmPoints.class);
+        points.add(BATHROOM);
+        System.out.println(points);
+        points.addAll(EnumSet.of(STAIR1,STAIR2,KITCHEN));
+        System.out.println(points);
+        points = EnumSet.allOf(AlarmPoints.class);
+        points.removeAll(EnumSet.of(STAIR1,STAIR2,KITCHEN));
+        System.out.println(points);
+        points.removeAll(EnumSet.range(OFFICE1,OFFICE4));
+        System.out.println(points);
+        points = EnumSet.complementOf(points);
+        System.out.println(points);
+    }
+}
